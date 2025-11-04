420153425038144e47e31b7f5ee5563b020ea4fc
Aditya Shukla <Shadowshukla76@gmail.com>
2025-11-05 01:03:15 +0530

    added service to create category

---
+++ src/services/category.service.js
@@ -0,0 +1,22 @@
+import { logger } from "../config/logger.config";
+import CategorySchema from "../models/Category.schema";
+import { AppError } from "../utils/AppError";
+
+export const CreateCategory = async (categoryData) => {
+  try {
+    const newCategory = new CategorySchema({
+      name: categoryData.name,
+      description: categoryData.description,
+      image: categoryData.image,
+      tax_applicable: categoryData.tax_applicable,
+      tax: categoryData.tax,
+      tax_type: categoryData.tax_type,
+    });
+    await newCategory.save();
+    return newCategory;
+  } catch (error) {
+    logger.error(`Failed to create category: ${error}`);
+    console.error(`Failed to create user: `, error);
+    throw new AppError(`Failed to create user: ${error}`, 500);
+  }
+};
